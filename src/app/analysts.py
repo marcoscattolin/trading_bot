d3cef5b6cfcdace148e2cd55615b1ae27ff93c62
Scattolin-Marco <scattolin.marco@bcg.com>
2024-10-03 19:22:20 +0200

    better orchestration

---
+++ src/app/analysts.py
@@ -0,0 +1,114 @@
+from abc import abstractmethod
+from langchain_openai import ChatOpenAI
+from src.config.config import conf
+from langchain_core.messages import HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+import asyncio
+from src.utils.logging import logger
+from lumibot.brokers import Alpaca
+from src.app.traders import AlpacaTrader
+
+class LLMAnalyst:
+
+    name = "LLMAnalyst"
+
+    def __init__(self):
+
+        chat = ChatOpenAI(
+            openai_api_key=conf.openai.api_key.get_secret_value(),
+            model="gpt-4o",
+            temperature=0.2
+        )
+
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    "You are a seasoned financial advisor expert in trading Stocks. You are here to suggest the "
+                    "best stocks to buy or sell depending on the latest news. When the user sends a message, you should "
+                    "reply with the best stock to buy based on the latest news. When you reply, just use the stock "
+                    "symbol, the action (buy/sell) and the reason why you suggest that action. Use '-' to separate the "
+                    "stock symbol, the action and the reason.  For example: AAPL - buy - reason. If the message is not "
+                    "relevant for trading stocks, just reply with '<not_relevant>'.",
+                ),
+                MessagesPlaceholder(variable_name="messages"),
+            ]
+        )
+
+        self.chain = prompt | chat
+
+    @abstractmethod
+    def _format_input(self, message):
+
+        # extract symbols from the data
+        symbols = ", ".join(message.get("symbols"))
+        headline = message.get("headline")
+        summary = message.get("summary")
+        message = f"{symbols} | {headline} | {summary} | "
+
+        return message
+
+    def _format_output(self, input_message):
+
+        # parse the result
+        if input_message == "<not_relevant>":
+            return None
+        else:
+
+            symbol, action, reason = input_message.content.split(" - ")
+            if action not in ["buy", "sell"]:
+                return None
+            else:
+                output_message = {
+                    "symbol": symbol,
+                    "action": action,
+                    "reason": reason,
+                }
+
+            return output_message
+
+    def analyze(self, message):
+
+        try:
+            result = self.chain.invoke(
+                {
+                    "messages": [
+                        HumanMessage(
+                            content=self._format_input(message)
+                        )
+                    ]
+                }
+            )
+
+            return self._format_output(result)
+
+        except:
+            # do nothing
+            return None
+
+
+if __name__ == "__main__":
+
+    analyst = LLMAnalyst()
+
+    data = [
+        {
+            "symbols": ["AAPL", "NVDA"],
+            "headline": "Apple shares are expected to boom in the following days.",
+            "summary": "Apple is expected to release a new product that will revolutionize the industry."
+        },
+        {
+            "symbols": ["AAPL", "NVDA"],
+            "headline": "Nvidia is declaring bankruptcy.",
+            "summary": "Nvidia is facing financial difficulties and is expected to declare bankruptcy."
+        },
+        {
+            "symbols": ["AAPL", "NVDA"],
+            "headline": "What is the secret to happiness?",
+            "summary": "The secret"
+        }
+        ]
+
+    for d in data:
+        result = analyst.analyze(d)
+        logger.info(result)
--- src/app/listeners.py
+++ src/app/listeners.py
@@ -1,18 +1,34 @@
 from alpaca.data.live import NewsDataStream
 from src.config.config import conf
-from src.app.handlers import LLMTradingHandler as Handler
 from src.utils.logging import logger
 
 
 class NewsListener:
 
-    def __init__(self, handle_func, symbols:[str]):
+    name = "NewsListener"
+
+    def __init__(self, symbols:[str]):
 
         self.wss_client = NewsDataStream(
             api_key=conf.alpaca_creds.api_key,
             secret_key=conf.alpaca_creds.secret_key.get_secret_value()
         )
-        self.wss_client.subscribe_news(handle_func, *symbols)
+        self.wss_client.subscribe_news(self.handler, *symbols)
+
+    async def handler(self, data):
+        # convert data to dict
+        message = dict(data)
+
+        # log message
+        logger.info(f"[{self.name}]: {message}")
+
+        # send to dispatcher
+        self.dispatch(message)
+
+    def dispatch(self, message):
+
+        # to be customized in inherited classes
+        logger.debug(f"Will dispatch message: {message}")
 
     def run(self):
         self.wss_client.run()
@@ -20,7 +36,5 @@ class NewsListener:
 
 if __name__ == "__main__":
 
-    logger.setLevel("WARNING")
-    handler = Handler().get_handler()
-    listener = NewsListener(handler, symbols=["*"])
+    listener = NewsListener(symbols=["*"])
     listener.run()
